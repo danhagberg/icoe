6bd61d5facfb17db98ab485951206d26cf16cc12
Dan Hagberg <sweep@digitaltsunami.net>
2021-03-02 08:54:21 -0800

    2 icoe-2 Model Principal Data.  Added model data for principal, auto, contact, and emergency contact.  Upgraded versions to match to fix issue with auth.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 import Amplify from "aws-amplify"
 import awsExports from "./aws-exports"
+
 Amplify.configure(awsExports)
 
 ReactDOM.render(
